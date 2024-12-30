663a8e5d015b3bb9a9fa0f6bae7c02d8893bea59
Moustafa <mosafehashf@gmail.com>
2024-12-30 22:26:19 +0000

    refactor(provider.tf): remove unnecessary duplicated lines

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-    required_version = ">= 1.2.0"
-    required_providers {
-        google = {
-        source  = "hashicorp/google"
-        version = "~> 4.0"
-        }
-    }
-}
-
 # Configure the Google Cloud provider
 provider "google" {
     project = var.project_id
