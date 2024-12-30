48bb10a9aac3608c3356e582975114346d365ea5
Moustafa <mosafehashf@gmail.com>
2024-12-30 20:17:33 +0000

    refactor(infra/variables.tf): adding image variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,21 +1,43 @@
 variable "project_id" {
+    description = "The GCP project ID"
     type        = string
-    description = "GCP project ID"
 }
 
 variable "region" {
+    description = "The region to deploy to"
     type        = string
-    description = "GCP region"
     default     = "us-central1"
 }
 
 variable "service_name" {
+    description = "The name of the Cloud Run service"
     type        = string
-    description = "Name of the Cloud Run service"
-    default     = "my-ner-service"
+    default     = "medical-entity-extraction"
 }
 
 variable "image_name" {
+    description = "The name of the container image"
     type        = string
-    description = "Full Docker image path (gcr.io/PROJECT_ID/IMAGE:TAG)"
+}
+
+variable "env_vars" {
+    description = "Environment variables to set in the container"
+    type        = map(string)
+    default = {
+        GCP_PROJECT_ID = null  
+        GCP_MODEL_NAME   = null  
+        GCP_LOCATION     = null  
+    }
+}
+
+variable "memory_limit" {
+    description = "Memory limit for the container"
+    type        = string
+    default     = "2Gi"
+}
+
+variable "cpu_limit" {
+    description = "CPU limit for the container"
+    type        = string
+    default     = "1"
 }
