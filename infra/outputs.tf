d399c423677452fc1b6666135d797e2fc72ffed0
Moustafa <mosafehashf@gmail.com>
2024-12-30 21:38:48 +0000

    fix(outputs.tf): update service URL output to reference the correct Cloud Run service for medical entity extraction

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
 output "service_url" {
-    value       = google_cloud_run_service.default.status[0].url
+    value       = google_cloud_run_service.medical_entity_extraction_service.status[0].url
     description = "The URL of the deployed service"
 }
