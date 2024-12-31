558ef011a2a67a345228014d2ad8100ea0299015
Moustafa <mosafehashf@gmail.com>
2024-12-31 20:54:10 +0000

    refactor(tests/test_app.py): update test cases for PDF extraction; replace hardcoded file paths with Pathlib, add file existence checks, and improve error handling for invalid file types

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,8 @@
+from pathlib import Path
 import pytest
 from fastapi.testclient import TestClient
 from src.app import app
 import os
-
-# TODO: test a simple i/o
-# TODO: test lower level functions
 
 client = TestClient(app)
 
@@ -14,19 +12,26 @@ def test_extract_empty_file():
     response = client.post("/api/v1/extract")
     assert (
         response.status_code == 422
-    )  # FastAPI's default error for missing required field
-
+    )
 
 def test_extract_invalid_file():
     # Test with non-PDF file
-    files = {"file": ("invalid.txt", b"Some text content", "text/plain")}
-    response = client.post("/api/v1/extract", files=files)
-    assert response.status_code == 400
+    test_pdf_path = Path(__file__).parent / "test_files" / "invalidExtention.txt"
+
+    # Skip if test file doesn't exist
+    if not os.path.exists(test_pdf_path):
+        pytest.skip("Test PDF file not found")
+
+    with open(test_pdf_path, "rb") as pdf_file:
+        files = {"file": ("invalidExtension.txt", pdf_file, "text/plain")}
+        response = client.post("/api/v1/extract", files=files)
+
+    assert response.status_code == 415
 
 
 def test_extract_valid_pdf():
     # Create a test PDF file path
-    test_pdf_path = os.path.join(os.path.dirname(__file__), "test_files", "valid.pdf")
+    test_pdf_path = Path(__file__).parent / "test_files" / "valid.pdf"
 
     # Skip if test file doesn't exist
     if not os.path.exists(test_pdf_path):
